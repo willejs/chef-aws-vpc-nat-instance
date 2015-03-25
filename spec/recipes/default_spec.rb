8c0d69604da685add054f731e90b4bb23ee93e70
willejs <williamejsalt@gmail.com>
2015-03-25 14:47:23 -0400

    added chefspec unit tests

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -2,7 +2,7 @@ require_relative '../spec_helper'
 
 describe 'aws-vpc-nat-instance::default' do
   let(:chef_run) { ChefSpec::ServerRunner.new.converge(described_recipe) }
- 
+
   it 'creates the sysctl forward rule' do
     expect(chef_run).to apply_sysctl_param('net.ipv4.ip_forward')
   end
