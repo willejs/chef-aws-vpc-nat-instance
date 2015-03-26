0338dcc8323208f40a0c849035aae789847e89d4
willejs <williamejsalt@gmail.com>
2015-03-26 10:04:50 -0400

    add interface attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,4 @@
 default['aws-vpc-nat-instance']['ipmasq_src'] = '10.0.0.0/8'
+default['aws-vpc-nat-instance']['interface'] = 'eth0'
+default['aws-vpc-nat-instance']['access_key_id'] = nil
+default['aws-vpc-nat-instance']['secret_access_key'] = nil
